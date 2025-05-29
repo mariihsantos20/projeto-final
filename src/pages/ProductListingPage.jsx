9d1cccf3ebacb9264cffc7acc826669ee6c90dff
Mariana Santos <mariihsantos20@gmail.com>
2025-05-29 18:34:03 -0300

    Atualização do ProdctListingPage

--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -1,6 +1,35 @@
+import Section from "../components/Section";
+import ProductListing from "../components/ProductListing";
+
 const ProductListingPage = () => {
-    return ( <>
-    </> );
+    return ( <div>
+<div>
+<Section>
+
+
+
+{/* Aqui vc coloca o Ordenar por e Filtrar por */}
+
+
+
+</Section>
+ </div>
+
+
+
+        {/* Cards de Produtos*/}
+   <div className="flex justify-around flex-wrap px-4 ml-8">
+  <Section 
+    title="Produtos em alta"
+    link={{ text: 'Ver todos -', href: '/novidades' }}
+  >
+    <div className="flex flex-wrap justify-around">
+      <ProductListing products={products} />
+    </div>
+  </Section>
+</div>
+</div>
+     );
 }
  
 export default ProductListingPage;
